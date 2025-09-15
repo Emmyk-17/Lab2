799848ad6ed840dbd0451d210baf3a3d5141cb69
EMMANUEL <eadeyemikings123@gmail.com>
2025-09-15 17:24:33 +0100

    added y

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,14 @@ public class Main {
     {
         Random gen = new Random();
         int x = gen.nextInt(10);
+        int y;
+
         System.out.format("This is x and its value is %d\n", x);
-        
+
+        y = x + gen.nextInt(4);
         x += 3;
         System.out.println("Hello world!");
+        System.out.println("this is y its value is " + y);
         System.out.format("This is x and its new value is %d\n", x);
     }
 }
