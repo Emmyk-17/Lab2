5bb6af5bdc200393db8d67fbcbf9b736f8f3d344
EMMANUEL <eadeyemikings123@gmail.com>
2025-09-15 17:11:07 +0100

    new lines of code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int x = 4;
+        x += 3;
         System.out.println("Hello world!");
+        System.out.format("This is x and its value is %d\n", x);
     }
 }
