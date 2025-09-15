5b5ead1a5e10b50850b749f5afdfb98a42e2c34b
Emmyk-17 <136089110+Emmyk-17@users.noreply.github.com>
2025-09-15 17:18:01 +0100

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args)
     {
-        int x = 4;
+        Random gen = new Random();
+        int x = gen.nextInt(10);
+        System.out.format("This is x and its value is %d\n", x);
+        
         x += 3;
         System.out.println("Hello world!");
-        System.out.format("This is x and its value is %d\n", x);
+        System.out.format("This is x and its new value is %d\n", x);
     }
 }
